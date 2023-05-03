66184d42b10cce744a61f060e3c3c12050b770bf
AlexeyIpatov <alexey.lift@gmail.com>
2023-05-03 21:26:10 +0300

    Initial commit

---
+++ src/MyFirstClass.java
@@ -0,0 +1,25 @@
+public class MyFirstClass {
+    public static void main(String[] args) {
+        byte myAgeByte = 26;
+        int myAgeInt = 26;
+        short myAgeShort = 26;
+        double myDouble = 26.2;
+        float myFloat = 1.5f;
+        char myChar = '\u2131';
+        int y = 10;
+        int x = 20;
+        System.out.println((double) x+y);
+        boolean myBool = true;
+
+        System.out.println(--x);
+        System.out.println(x++);
+        System.out.println(x--);
+        System.out.println(x);
+        boolean result = x!=y;
+        System.out.println((x<y) || (x>y));
+        System.out.println((x>y) ? "YES" : "NO");
+        String name ="ALEXEY";
+        // большая буква - ссылочный тип, а маленькая - примитивный
+        //ЧТо такой JDK
+    }
+}
